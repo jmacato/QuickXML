3732d1deb55eb83d4f45648df54f4db0680524f8
Jumar Macato <hikari.netto23@gmail.com>
2018-06-21 09:45:04 +0800

    Initial Commit

---
+++ QuickXML.Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuickXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
